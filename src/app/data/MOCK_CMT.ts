d749215dde6f06f5204e47db5eb1af3c5086aa01
지연우 <yanoo8184@gmail.com>
2024-05-16 15:16:13 +0900

    mock 데이터 만들기

---
+++ src/app/data/MOCK_CMT.ts
@@ -0,0 +1,32 @@
+export const MOCK_CMT = [
+    {
+        id:1,
+        title: 'test1',
+        imageUrl: 'ddd',
+        route: '/buttons'
+      },
+      {
+        id:2,
+        title: 'test2',
+        imageUrl: 'ddd',
+        route: 'buttons'
+      },
+      {
+        id:3,
+        title: 'test3',
+        imageUrl: 'ddd',
+        route: 'buttons'
+      },
+      {
+        id:4,
+        title: 'test4',
+        imageUrl: 'ddd',
+        route: 'buttons'
+      },
+      {
+        id:5,
+        title: 'test5',
+        imageUrl: 'ddd',
+        route: 'buttons'
+      }
+]
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink  } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CType } from '../../interface/component-type.interface';
+import { MOCK_CMT } from '../../data/MOCK_CMT';
 
 @Component({
   selector: 'app-main',
@@ -15,37 +16,12 @@ import { CType } from '../../interface/component-type.interface';
   `
 })
 export class MainComponent {
+  
   componentBox = 'flex flex-col gap-[0.5rem] items-center bg-slate-100 p-[1.5rem] rounded-[1rem]'
-  componentList: CType[] = [
-    {
-      id:1,
-      title: 'test1',
-      imageUrl: 'ddd',
-      route: 'buttons'
-    },
-    {
-      id:2,
-      title: 'test2',
-      imageUrl: 'ddd',
-      route: 'buttons'
-    },
-    {
-      id:3,
-      title: 'test3',
-      imageUrl: 'ddd',
-      route: 'buttons'
-    },
-    {
-      id:4,
-      title: 'test4',
-      imageUrl: 'ddd',
-      route: 'buttons'
-    },
-    {
-      id:5,
-      title: 'test5',
-      imageUrl: 'ddd',
-      route: 'buttons'
-    }
-  ]
+  /**
+   * 삽입될 컴포넌트 리스트 목데이터 할당
+   * @CType 컴포넌트 타입의 인터페이스 정의
+   * @MOCK_CMT 컴포넌트 리스트 목데이터
+   */
+  componentList: CType[] = MOCK_CMT 
 }
